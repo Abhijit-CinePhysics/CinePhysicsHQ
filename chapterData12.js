b112e3d590e6c86ea9ee742417f9e7a24566ac82
Abhijit-CinePhysics <singhabhijit327@gmail.com>
2026-06-01 21:45:04 +0530

    Create chapterData12

---
+++ chapterData12.js
@@ -0,0 +1,217 @@
+const class12Data = {
+
+"Electric Charges and Fields": [
+"Electric Charges",
+"Conservation of Charge",
+"Coulomb's Law",
+"Superposition Principle",
+"Continuous Charge Distribution",
+"Electric Field",
+"Electric Field Due to Point Charge",
+"Electric Field Lines",
+"Electric Dipole",
+"Electric Field Due to Dipole",
+"Torque on Dipole",
+"Electric Flux",
+"Gauss's Theorem",
+"Field Due to Infinite Wire",
+"Field Due to Infinite Plane Sheet",
+"Field Due to Spherical Shell"
+],
+
+"Electrostatic Potential and Capacitance": [
+"Electric Potential",
+"Potential Difference",
+"Potential Due to Point Charge",
+"Potential Due to Dipole",
+"Potential Due to System of Charges",
+"Equipotential Surfaces",
+"Potential Energy",
+"Conductors",
+"Insulators",
+"Free Charges",
+"Bound Charges",
+"Dielectrics",
+"Electric Polarization",
+"Capacitors",
+"Capacitance",
+"Series Combination",
+"Parallel Combination",
+"Parallel Plate Capacitor",
+"Energy Stored in Capacitor"
+],
+
+"Current Electricity": [
+"Electric Current",
+"Flow of Charges",
+"Drift Velocity",
+"Mobility",
+"Ohm's Law",
+"V-I Characteristics",
+"Electrical Energy",
+"Electrical Power",
+"Resistivity",
+"Conductivity",
+"Temperature Dependence of Resistance",
+"Internal Resistance",
+"EMF",
+"Cells in Series",
+"Cells in Parallel",
+"Kirchhoff's Rules",
+"Wheatstone Bridge"
+],
+
+"Moving Charges and Magnetism": [
+"Oersted Experiment",
+"Magnetic Field",
+"Biot-Savart Law",
+"Circular Loop",
+"Ampere's Law",
+"Long Straight Conductor",
+"Solenoid",
+"Lorentz Force",
+"Force on Moving Charge",
+"Force on Current Carrying Conductor",
+"Parallel Conductors",
+"Definition of Ampere",
+"Torque on Current Loop",
+"Magnetic Dipole Moment",
+"Moving Coil Galvanometer",
+"Conversion to Ammeter",
+"Conversion to Voltmeter"
+],
+
+"Magnetism and Matter": [
+"Bar Magnet",
+"Equivalent Solenoid",
+"Magnetic Dipole",
+"Magnetic Field Lines",
+"Torque on Magnetic Dipole",
+"Diamagnetic Materials",
+"Paramagnetic Materials",
+"Ferromagnetic Materials",
+"Magnetisation",
+"Temperature Effects"
+],
+
+"Electromagnetic Induction": [
+"Electromagnetic Induction",
+"Faraday's Laws",
+"Induced EMF",
+"Induced Current",
+"Lenz's Law",
+"Self Induction",
+"Mutual Induction"
+],
+
+"Alternating Current": [
+"Alternating Current",
+"Peak Value",
+"RMS Value",
+"Reactance",
+"Impedance",
+"LCR Circuit",
+"Phasors",
+"Resonance",
+"Power in AC Circuit",
+"Power Factor",
+"Wattless Current",
+"AC Generator",
+"Transformer"
+],
+
+"Electromagnetic Waves": [
+"Displacement Current",
+"Electromagnetic Waves",
+"Characteristics of EM Waves",
+"Transverse Nature",
+"Radio Waves",
+"Microwaves",
+"Infrared",
+"Visible Light",
+"Ultraviolet",
+"X-Rays",
+"Gamma Rays"
+],
+
+"Ray Optics and Optical Instruments": [
+"Reflection",
+"Spherical Mirrors",
+"Mirror Formula",
+"Refraction",
+"Total Internal Reflection",
+"Optical Fibres",
+"Refraction at Spherical Surfaces",
+"Lenses",
+"Thin Lens Formula",
+"Lens Maker Formula",
+"Magnification",
+"Power of Lens",
+"Combination of Lenses",
+"Prism",
+"Microscope",
+"Astronomical Telescope"
+],
+
+"Wave Optics": [
+"Wavefront",
+"Huygens Principle",
+"Reflection Using Wavefronts",
+"Refraction Using Wavefronts",
+"Interference",
+"YDSE",
+"Fringe Width",
+"Coherent Sources",
+"Diffraction",
+"Single Slit Diffraction",
+"Central Maximum"
+],
+
+"Dual Nature of Radiation and Matter": [
+"Photoelectric Effect",
+"Hertz Experiment",
+"Lenard Experiment",
+"Einstein Photoelectric Equation",
+"Particle Nature of Light",
+"de Broglie Hypothesis",
+"Matter Waves"
+],
+
+"Atoms": [
+"Alpha Particle Scattering",
+"Rutherford Model",
+"Bohr Model",
+"Radius of Orbit",
+"Velocity of Electron",
+"Energy of Electron",
+"Hydrogen Spectrum"
+],
+
+"Nuclei": [
+"Composition of Nucleus",
+"Nuclear Size",
+"Nuclear Force",
+"Mass Defect",
+"Binding Energy",
+"Binding Energy Curve",
+"Nuclear Fission",
+"Nuclear Fusion"
+],
+
+"Semiconductor Electronics": [
+"Energy Bands",
+"Conductors",
+"Semiconductors",
+"Insulators",
+"Intrinsic Semiconductors",
+"Extrinsic Semiconductors",
+"P-Type Semiconductor",
+"N-Type Semiconductor",
+"PN Junction",
+"Forward Bias",
+"Reverse Bias",
+"Diode Characteristics",
+"Rectifier"
+]
+
+};
