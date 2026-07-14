dd8b900afedfb8283ab40ff12231f0d493e984d8
Abhijit-CinePhysics <singhabhijit327@gmail.com>
2026-07-14 12:36:52 +0530

    Create chapterData10.js

---
+++ chapterData10.js
@@ -0,0 +1,60 @@
+const class10Data = {
+  "Light - Reflection and Refraction": [
+    "Reflection of Light",
+    "Spherical Mirrors",
+    "Mirror Formula",
+    "Magnification",
+    "Refraction of Light",
+    "Refractive Index",
+    "Snell's Law",
+    "Lens Formula",
+    "Power of Lens"
+  ],
+
+  "Human Eye and the Colourful World": [
+    "Human Eye",
+    "Power of Accommodation",
+    "Defects of Vision",
+    "Correction of Vision",
+    "Refraction Through Prism",
+    "Atmospheric Refraction",
+    "Scattering of Light",
+    "Tyndall Effect"
+  ],
+
+  "Electricity": [
+    "Electric Charge",
+    "Electric Current",
+    "Electric Potential",
+    "Potential Difference",
+    "Ohm's Law",
+    "Resistance",
+    "Combination of Resistors",
+    "Heating Effect of Current",
+    "Electric Power"
+  ],
+
+  "Magnetic Effects of Electric Current": [
+    "Magnetic Field",
+    "Field Lines",
+    "Magnetic Field Around Conductor",
+    "Right-Hand Thumb Rule",
+    "Force on Current Carrying Conductor",
+    "Electric Motor",
+    "Electromagnetic Induction",
+    "Electric Generator",
+    "Domestic Electric Circuits"
+  ],
+
+  "Sources of Energy": [
+    "Conventional Sources of Energy",
+    "Thermal Power Plant",
+    "Hydroelectric Power Plant",
+    "Renewable Sources",
+    "Solar Energy",
+    "Wind Energy",
+    "Biogas",
+    "Nuclear Energy",
+    "Environmental Consequences"
+  ]
+};
