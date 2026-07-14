15650a3840dec80893e0c4283fb0d6a983ae9c31
Abhijit-CinePhysics <singhabhijit327@gmail.com>
2026-07-14 12:35:55 +0530

    Create chapterdata9.js

---
+++ chapterdata9.js
@@ -0,0 +1,57 @@
+const class9Data = {
+  "Motion": [
+    "Motion",
+    "Distance and Displacement",
+    "Speed",
+    "Velocity",
+    "Acceleration",
+    "Equations of Motion",
+    "Graphical Representation of Motion",
+    "Uniform Circular Motion"
+  ],
+
+  "Force and Laws of Motion": [
+    "Force",
+    "Balanced and Unbalanced Forces",
+    "Newton's First Law",
+    "Momentum",
+    "Newton's Second Law",
+    "Newton's Third Law",
+    "Law of Conservation of Momentum"
+  ],
+
+  "Gravitation": [
+    "Universal Law of Gravitation",
+    "Gravitational Force",
+    "Free Fall",
+    "Acceleration Due to Gravity",
+    "Mass and Weight",
+    "Thrust and Pressure",
+    "Buoyancy",
+    "Archimedes' Principle",
+    "Relative Density"
+  ],
+
+  "Work and Energy": [
+    "Work",
+    "Positive and Negative Work",
+    "Energy",
+    "Kinetic Energy",
+    "Potential Energy",
+    "Law of Conservation of Energy",
+    "Power",
+    "Commercial Unit of Energy"
+  ],
+
+  "Sound": [
+    "Production of Sound",
+    "Propagation of Sound",
+    "Characteristics of Sound",
+    "Speed of Sound",
+    "Reflection of Sound",
+    "Echo",
+    "Reverberation",
+    "Applications of Ultrasound",
+    "Human Ear"
+  ]
+};
